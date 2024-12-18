39cc411d30ac400c530c5ae8219aff84fa75ee2f
Michel Atucha <zp1ke@outlook.com>
2024-12-18 18:05:15 -0500

    font & splash

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,16 +1,19 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopster/utils/theme/extension.dart';
 
 class AppTheme {
   AppTheme._();
 
   static const _flexScheme = FlexScheme.indigoM3;
+  static final _fontFamily = GoogleFonts.notoSans().fontFamily;
   static final _visualDensity = FlexColorScheme.comfortablePlatformDensity;
 
   static ThemeData light = FlexThemeData.light(
     scheme: _flexScheme,
     useMaterial3: true,
+    fontFamily: _fontFamily,
     visualDensity: _visualDensity,
     extensions: [
       AppThemeColors.light,
@@ -20,6 +23,7 @@ class AppTheme {
   static ThemeData dark = FlexThemeData.dark(
     scheme: _flexScheme,
     useMaterial3: true,
+    fontFamily: _fontFamily,
     visualDensity: _visualDensity,
     extensions: [
       AppThemeColors.dark,
---
+++ scripts/splash.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# argument options: create or remove
+dart run flutter_native_splash:"$1" --path=splash.yaml
